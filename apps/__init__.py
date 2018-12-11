eb4393de12a7a7acb16518fc228c7472abbf3b09
Sergi Delgado <sergi.delgado.s@gmail.com>
2018-12-11 10:51:19 +0000

    Updates all numeric values loaded from apps config file

--- apps/__init__.py
+++ apps/__init__.py
@@ -12,10 +12,10 @@ COINSCOPED_API_PORT = int(cfg.get('general', 'coinscoped_api_port'))
 MAINNET_PORT = 8333
 TESTNET_PORT = 18333
 HALF_TARGET = 0.5
-TARGET = cfg.get('nethealth', 'target')
-WIPE_TIME = cfg.get('nethealth', 'wipe_time')
-WIPE_DELAY = cfg.get('nethealth', 'wipe_delay')
+TARGET = float(cfg.get('nethealth', 'target'))
+WIPE_TIME = int(cfg.get('nethealth', 'wipe_time'))
+WIPE_DELAY = int(cfg.get('nethealth', 'wipe_delay'))
 
 CARBON_SERVER = cfg.get('nethealth', 'carbon_server')
-CARBON_PICKLE_PORT = cfg.get('nethealth', 'carbon_pickle_port')
-UPDATE_DELAY = cfg.get('nethealth', 'update_delay')
+CARBON_PICKLE_PORT = int(cfg.get('nethealth', 'carbon_pickle_port'))
+UPDATE_DELAY = int(cfg.get('nethealth', 'update_delay'))
