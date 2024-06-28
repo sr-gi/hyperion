c71c5555ccedfcb9a6d4997671e582faf060cb16
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2024-06-28 13:15:46 -0400

    lib: Adds data structures to deal with delayed messages and finishes connecting the data reception flow

--- hyper-lib/src/node.rs
+++ hyper-lib/src/node.rs
@@ -1,9 +1,11 @@
 use std::collections::{HashMap, HashSet, VecDeque};
+use std::sync::{Arc, Mutex};
+use std::time::Instant;
 
 pub type NodeId = u32;
 pub type TxId = u32;
 
-#[derive(Clone)]
+#[derive(Copy, Clone)]
 pub enum NetworkMessage {
     INV(TxId),
     GETDATA(TxId),
@@ -57,6 +59,7 @@ impl std::fmt::Display for NetworkMessage {
 pub struct Peer {
     known_transactions: HashSet<TxId>,
     message_queue: VecDeque<NetworkMessage>,
+    inv_queue: VecDeque<NetworkMessage>,
 }
 
 impl Peer {
@@ -64,6 +67,7 @@ impl Peer {
         Peer {
             known_transactions: HashSet::new(),
             message_queue: VecDeque::new(),
+            inv_queue: VecDeque::new(),
         }
     }
 
@@ -76,7 +80,11 @@ impl Peer {
     }
 
     fn enqueue_message(&mut self, msg: NetworkMessage) {
-        self.message_queue.push_back(msg)
+        if msg.is_inv() {
+            self.inv_queue.push_back(msg)
+        } else {
+            self.message_queue.push_back(msg)
+        }
     }
 }
 
@@ -84,8 +92,10 @@ impl Peer {
 pub struct Node {
     node_id: NodeId,
     is_reachable: bool,
-    in_peers: HashMap<NodeId, Peer>,
-    out_peers: HashMap<NodeId, Peer>,
+    in_peers: HashMap<NodeId, Arc<Mutex<Peer>>>,
+    out_peers: HashMap<NodeId, Arc<Mutex<Peer>>>,
+    requested_transactions: Arc<Mutex<HashSet<TxId>>>,
+    delayed_requests: Arc<Mutex<HashMap<TxId, (NodeId, Instant)>>>,
     known_transactions: HashSet<TxId>,
 }
 
@@ -96,32 +106,32 @@ impl Node {
             is_reachable,
             in_peers: HashMap::new(),
             out_peers: HashMap::new(),
+            requested_transactions: Arc::new(Mutex::new(HashSet::new())),
+            delayed_requests: Arc::new(Mutex::new(HashMap::new())),
             known_transactions: HashSet::new(),
         }
     }
 
-    fn get_peer(&self, peer_id: NodeId) -> Option<&Peer> {
+    fn get_peer(&self, peer_id: &NodeId) -> Option<&Arc<Mutex<Peer>>> {
         self.out_peers
             .get(&peer_id)
-            .or_else(|| self.in_peers.get(&peer_id))
-    }
-
-    fn get_peer_mut(&mut self, peer_id: NodeId) -> Option<&mut Peer> {
-        self.out_peers
-            .get_mut(&peer_id)
-            .or_else(|| self.in_peers.get_mut(&peer_id))
+            .or_else(|| self.in_peers.get(peer_id))
     }
 
     pub fn get_id(&self) -> NodeId {
         self.node_id
     }
 
-    pub fn get_inbounds(&self) -> &HashMap<NodeId, Peer> {
+    pub fn get_inbounds(&self) -> &HashMap<NodeId, Arc<Mutex<Peer>>> {
         &self.in_peers
     }
 
-    pub fn get_outbounds(&self) -> &HashMap<NodeId, Peer> {
+    pub fn get_outbounds(&self) -> &HashMap<NodeId, Arc<Mutex<Peer>>> {
         &self.out_peers
+    }
+
+    pub fn is_inbounds(&self, peer_id: &NodeId) -> bool {
+        self.in_peers.contains_key(peer_id)
     }
 
     pub fn connect(&mut self, peer_id: NodeId) {
@@ -130,7 +140,9 @@ impl Node {
             "Peer {peer_id} is already connected to us"
         );
         assert!(
-            self.out_peers.insert(peer_id, Peer::new()).is_none(),
+            self.out_peers
+                .insert(peer_id, Arc::new(Mutex::new(Peer::new())))
+                .is_none(),
             "We ({}) are already connected to {peer_id}",
             self.node_id
         );
@@ -148,7 +160,9 @@ impl Node {
             self.node_id
         );
         assert!(
-            self.in_peers.insert(peer_id, Peer::new()).is_none(),
+            self.in_peers
+                .insert(peer_id, Arc::new(Mutex::new(Peer::new())))
+                .is_none(),
             "Peer {peer_id} is already connected to us"
         );
     }
@@ -161,44 +175,124 @@ impl Node {
         self.known_transactions.insert(txid);
     }
 
+    pub fn create_new_transaction(&mut self, txid: TxId) {
+        self.add_known_transaction(txid);
+        let inv = NetworkMessage::INV(txid);
+
+        let mut peers: Vec<u32> = self.in_peers.keys().cloned().collect();
+        peers.extend(self.out_peers.keys());
+        for peer_id in peers {
+            self.send_message_to(inv, peer_id)
+        }
+    }
+
+    fn add_request(&self, txid: TxId, peer_id: NodeId) -> bool {
+        // Transactions are only requested from a single peer (assuming honest behavior)
+        // Inbound peers are de-prioritized. If an outbound peer announces a transaction
+        // and an inbound peer request is in delayed stage, the inbounds will be dropped and
+        // the outbound will be processed
+        let mut delayed_requests = self.delayed_requests.lock().unwrap();
+        let mut requested_transactions = self.requested_transactions.lock().unwrap();
+
+        if !self.knows_transactions(&txid) && !requested_transactions.contains(&txid) {
+            if self.is_inbounds(&peer_id) {
+                if !delayed_requests.contains_key(&txid) {
+                    delayed_requests.insert(txid, (peer_id, Instant::now()));
+                    log::info!(
+                        "(Node {}) Delaying getdata for transaction request (txid: {txid:x}) to peer {peer_id}",
+                        self.node_id
+                    )
+                }
+            } else {
+                requested_transactions.insert(txid);
+                delayed_requests.remove(&txid);
+                return true;
+            }
+        } else {
+            log::info!(
+                "(Node {}) Already known transaction (txid: {txid:x}), not requesting to (peer_id: {peer_id})",
+                self.node_id
+            )
+        }
+        return false;
+    }
+
+    fn remove_request(&self, txid: TxId) {
+        let mut requested_transactions = self.requested_transactions.lock().unwrap();
+        let delayed_requests = self.delayed_requests.lock().unwrap();
+        assert!(!delayed_requests.contains_key(&txid));
+        assert!(requested_transactions.remove(&txid));
+    }
+
     pub fn send_message_to(&mut self, msg: NetworkMessage, peer_id: NodeId) {
-        if let Some(peer) = self.get_peer_mut(peer_id) {
+        if let Some(wrapped_peer) = self.get_peer(&peer_id) {
+            let peer = &mut wrapped_peer.lock().unwrap();
             let txid = *msg.inner();
+            let we_know_tx = self.knows_transactions(&txid);
             if msg.is_inv() {
                 // INVs for the same transaction can get crossed. If this happens break the cycle.
+                assert!(we_know_tx, "Trying to announce a transaction we should't know about (txid: {txid:x}) to a peer (peer_id: {peer_id})");
                 if peer.knows_transactions(&txid) {
                     return;
                 }
+                peer.enqueue_message(msg);
+                log::info!("(Node {}) Scheduling {msg} to peer {peer_id}", self.node_id);
             } else if msg.is_get_data() {
-                assert!(peer.knows_transactions(&txid), "Trying to request a transaction (txid: {txid}) from a peers that doesn't know about it (peer_id: {peer_id})");
+                assert!(!we_know_tx, "Trying to request a transaction we already know about (txid: {txid:x}) from a peer (peer_id: {peer_id})");
+                assert!(peer.knows_transactions(&txid), "Trying to request a transaction (txid: {txid:x}) from a peer that shouldn't know about it (peer_id: {peer_id})");
+                if !self.add_request(txid, peer_id) {
+                    return;
+                }
+                peer.enqueue_message(msg);
+                log::info!("(Node {}) Sending {msg} to peer {peer_id}", self.node_id);
             } else {
-                assert!(!peer.knows_transactions(&txid), "Trying to send a transaction (txid: {txid}) from a peers that already knows about it (peer_id: {peer_id})");
+                assert!(we_know_tx, "Trying to send a transaction we should't know about (txid: {txid:x}) to a peer (peer_id: {peer_id})");
+                assert!(!peer.knows_transactions(&txid), "Trying to send a transaction (txid: {txid:x}) to a peer that already should know about it (peer_id: {peer_id})");
+                peer.enqueue_message(msg);
                 peer.add_known_transaction(txid);
+                log::info!("(Node {}) Sending {msg} to peer {peer_id}", self.node_id);
             }
-            log::info!("Sending {msg} to peer {peer_id}");
-            peer.enqueue_message(msg);
         }
     }
 
     pub fn receive_message_from(&mut self, msg: NetworkMessage, peer_id: NodeId) {
-        let txid = *msg.inner();
-        let we_know_tx = self.knows_transactions(&txid);
-        log::info!("Received {msg} from peer {peer_id}");
-        if let Some(peer) = self.get_peer_mut(peer_id) {
+        let mut response = None;
+
+        log::info!("(Node {}) Received {msg} from peer {peer_id}", self.node_id);
+        if let Some(wrapped_peer) = self.get_peer(&peer_id) {
+            let peer = &mut wrapped_peer.lock().unwrap();
+            let txid = *msg.inner();
+            let we_know_tx = self.knows_transactions(&txid);
             if msg.is_inv() {
                 peer.add_known_transaction(txid);
+                // We only request transactions that we don't know about
                 if !we_know_tx {
-                    log::info!("Transaction unknown. Sending getdata to peer {peer_id}");
-                    peer.enqueue_message(NetworkMessage::GETDATA(txid));
+                    log::info!(
+                        "(Node {}) Transaction unknown. Scheduling getdata to peer {peer_id}",
+                        self.node_id
+                    );
+                    response = Some((NetworkMessage::GETDATA(txid), peer_id));
                 }
             } else if msg.is_get_data() {
-                assert!(!peer.knows_transactions(&txid), "Received a transaction request (txid: {txid}) from a node that already knows about it (peer_id {peer_id})");
-                log::info!("Sending tx(txid: {txid}) to peer {peer_id}");
-                peer.enqueue_message(NetworkMessage::TX(txid));
+                assert!(we_know_tx, "Received transaction request for a transaction we don't know of (txid: {txid:x}) from a peer (peer_id: {peer_id})");
+                assert!(!peer.knows_transactions(&txid), "Received a transaction request (txid: {txid:x}) from a peer that should already know about it (peer_id {peer_id})");
+                log::info!(
+                    "(Node {}) Sending tx(txid: {txid}) to peer {peer_id}",
+                    self.node_id
+                );
+                response = Some((NetworkMessage::TX(txid), peer_id));
             } else {
-                assert!(peer.knows_transactions(&txid), "Received a transaction (txid: {txid}) from a node that shouldn't know about it (peer_id {peer_id})");
-                self.add_known_transaction(txid);
+                assert!(!we_know_tx, "Received a transaction we already know of (txid: {txid:x}) from a peer (peer_id: {peer_id})");
+                assert!(peer.knows_transactions(&txid), "Received a transaction (txid: {txid:x}) from a node that shouldn't know about it (peer_id {peer_id})");
+                self.remove_request(txid);
             }
+        }
+        if let Some((msg, peer_id)) = response {
+            self.send_message_to(msg, peer_id)
+        }
+
+        if msg.is_tx() {
+            self.add_known_transaction(*msg.inner());
         }
     }
 }
