3786baac55e748ba029b30121888306c165b8fcd
Sergi Delgado Segura <sergi.delgado.s@gmail.com>
2024-10-24 14:48:52 -0400

    lib: Makes the simulator simulate a single transaction
    The simulator was initially designed to allow simulating sending multiple transactions at the same time.
    This, however, means having to pass dynamically allocated vectors around and store them in the priority_queue,
    which greatly affects the performance of the simulation. We were, however, simulating sending a single transaction,
    meaning that a full re-design to get rid of all vector instances could be perform to get better performance while
    still being able to use the simulator for our purposes.
    
    This PR gets rid of all vectors in NetworkMessage, plus of the concept of TxId. Given we are sending a single transaction,
    a boolean can be used instead. This therefore also changes the way the Erlay messages work. Instead of performing differences
    between sets (of 1 or 0 transactions) now we perform differences between booleans, which is equivalent.

--- hyper-lib/src/txreconciliation.rs
+++ hyper-lib/src/txreconciliation.rs
@@ -1,28 +1,23 @@
-use hashbrown::HashSet;
-
-use itertools::Itertools;
-
-use crate::TxId;
-
 pub type ShortID = u32;
 
-// This is a hack. A sketch is really built using Minisketch. However, this is not necessary for the simulator.
-// The only thing we need to know is what transactions a node knows, and what is the size of the difference between that
-// and the set of transaction its peer knows. The difference can be computed on the fly, but it is stored here so we can keep
-// track of the size of the message for statistics.
-#[derive(Clone, PartialEq, Eq, Hash, Debug)]
+/// This is a hack. A sketch is really built using Minisketch. However, this is not necessary for the simulator.
+/// The only thing we need to know is whether the node knows the transaction, and what is the size of the difference
+/// between that and the set of transaction its peer knows (1 or 0, given we are simulating a single transactions).
+/// The difference can be computed on the fly, but it is stored here so we can keep
+/// track of the size of the message for statistics.
+#[derive(Clone, Copy, PartialEq, Eq, Hash, Debug)]
 pub struct Sketch {
-    tx_set: Vec<ShortID>,
+    tx_set: bool,
     d: usize,
 }
 
 impl Sketch {
-    pub fn new(tx_set: Vec<TxId>, d: usize) -> Self {
+    pub fn new(tx_set: bool, d: usize) -> Self {
         Self { tx_set, d }
     }
 
-    pub fn get_tx_set(&self) -> &Vec<TxId> {
-        &self.tx_set
+    pub fn get_tx_set(&self) -> bool {
+        self.tx_set
     }
 
     pub fn get_size(&self) -> usize {
@@ -36,14 +31,12 @@ pub struct TxReconciliationState {
     is_initiator: bool,
     /// Whether we are currently reconciling with this peer or not
     is_reconciling: bool,
-    /// Set of transactions to be reconciled with this peer (using Erlay).
-    /// Normally, ShortIDs would be 32 bits as opposed to 256-bit TxIds. However, we are already simplifying
-    /// TxIds to be 32-bit, so here it'd be a one-to-one mapping
-    recon_set: HashSet<ShortID>,
-    // Set of transactions to be added to the reconciliation set on the next trickle. These are still unrequestable for
-    // privacy reasons (to prevent transaction proving), transactions became available once they would have been announced
-    // via fanout (on the next trickle).
-    delayed_set: HashSet<ShortID>,
+    /// Whether the simulated transaction is in the reconciliation set
+    recon_set: bool,
+    /// Whether the simulated transaction is in pending to be added to the reconciliation set the next trickle.
+    /// These is still unrequestable for privacy reasons (to prevent transaction proving), the transaction will became
+    /// available once it would have been announced via fanout (on the next trickle).
+    delayed_set: bool,
 }
 
 impl TxReconciliationState {
@@ -51,36 +44,50 @@ impl TxReconciliationState {
         Self {
             is_initiator,
             is_reconciling: false,
-            recon_set: HashSet::new(),
-            delayed_set: HashSet::new(),
+            recon_set: false,
+            delayed_set: false,
         }
     }
 
-    pub fn clear(&mut self) -> HashSet<ShortID> {
+    pub fn clear(&mut self, include_delayed: bool) -> bool {
+        // The transaction cannot be in both sets at the same time
+        assert!(!(self.recon_set && self.delayed_set));
+
+        let recon_set = self.recon_set;
         self.is_reconciling = false;
-        self.recon_set.drain().collect()
+        self.recon_set = false;
+
+        if include_delayed {
+            self.delayed_set = false;
+        }
+
+        recon_set
     }
 
     pub fn is_initiator(&self) -> bool {
         self.is_initiator
     }
 
-    pub fn add_tx(&mut self, txid: TxId) -> bool {
-        self.delayed_set.insert(txid)
+    pub fn add_tx(&mut self) -> bool {
+        let r = !self.delayed_set;
+        self.delayed_set = true;
+
+        r
     }
 
-    /// Removes a transaction from the reconciliation set. This may happen if a peer has announced a transaction that we
+    /// Removes the transaction from the reconciliation set. This may happen if a peer has announced the transaction that we
     /// were planing to reconcile with them. Notice that, if this happens after creating a snapshot, the reconciliation will
-    /// result in one additional item on the exchanged INV (belonging to this transaction). This is equivalent to two INVs crossing,
-    /// and AFAIK, there's nothing we can do about it
-    pub fn remove_tx(&mut self, txid: &TxId) {
-        self.delayed_set.remove(txid);
-        self.recon_set.remove(txid);
+    /// result in one additional INV (belonging to this transaction). This is equivalent to two INVs crossing, and AFAIK,
+    /// there's nothing we can do about it
+    pub fn remove_tx(&mut self) {
+        self.delayed_set = false;
+        self.recon_set = false;
     }
 
     // Make delayed transactions available for reconciliation
     pub fn make_delayed_available(&mut self) {
-        self.recon_set.extend(self.delayed_set.drain());
+        self.recon_set = self.delayed_set;
+        self.delayed_set = false;
     }
 
     pub fn set_reconciling(&mut self) {
@@ -91,41 +98,44 @@ impl TxReconciliationState {
         self.is_reconciling
     }
 
-    pub fn get_recon_set(&self) -> &HashSet<ShortID> {
-        &self.recon_set
+    pub fn get_recon_set(&self) -> bool {
+        self.recon_set
     }
 
-    pub fn compute_sketch(&mut self, their_txs: Vec<TxId>) -> Sketch {
-        // Given predicting q is hard in a short simulation, we exchange the collection of transactions to be reconciled
-        // between the two parties. Here, q is computed as the difference between the two collections (local and remote)
-        // And a figurative Sketch is created
+    pub fn compute_sketch(&self, they_know_tx: bool) -> Sketch {
+        // q cannot be easily predicted in a short simulation, however it is needed to size the sketch properly.
+        // As a workaround, the sketches exchanges by the simulator are not really sketches, but knowledge of whether
+        // the sender knows the given transaction. q can be scaled down if needed to mimic scenarios where the sketch
+        // exchange is not perfectly efficient
         let local_set = self.get_recon_set();
-        let remote_set = HashSet::from_iter(their_txs);
-        let q = local_set.symmetric_difference(&remote_set).count();
+        let remote_set = they_know_tx;
+        // We can compute the size of the diff as int(A XOR B)
         // TODO: Scale q if required so the predicted difference is not always 100% accurate
-        Sketch::new(local_set.iter().copied().collect_vec(), q)
+        let q = (local_set ^ remote_set) as usize;
+        Sketch::new(local_set, q)
     }
 
-    pub fn compute_sketch_diff(&self, sketch: Sketch) -> (Vec<u32>, Vec<u32>) {
-        let local_set: HashSet<&u32> = HashSet::from_iter(self.get_recon_set());
-        let remote_set = HashSet::from_iter(sketch.get_tx_set());
+    pub fn compute_sketch_diff(&self, sketch: Sketch) -> (bool, bool) {
+        let local_set = self.get_recon_set();
+        let remote_set = sketch.get_tx_set();
 
-        // We care about what we are missing, that's what we send to our peer
-        let our_diff: Vec<u32> = remote_set.difference(&local_set).map(|x| **x).collect_vec();
-        let their_diff = local_set.difference(&remote_set).map(|x| **x).collect_vec();
+        // A XOR B to see if there is a diff
+        let diff = local_set ^ remote_set;
+        // If there us a diff, each end's diff is whether the other end knows the transaction
+        let local_diff = diff && remote_set;
+        let remote_diff = diff && local_set;
 
-        (our_diff, their_diff)
+        (local_diff, remote_diff)
     }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
-    use crate::test::get_random_txid;
 
     impl TxReconciliationState {
-        pub(crate) fn get_delayed_set(&self) -> &HashSet<ShortID> {
-            &self.delayed_set
+        pub(crate) fn get_delayed_set(&self) -> bool {
+            self.delayed_set
         }
     }
 
@@ -134,103 +144,90 @@ mod test {
         let mut tx_recon_state = TxReconciliationState::new(true);
         tx_recon_state.set_reconciling();
         assert!(tx_recon_state.is_reconciling());
-        assert!(tx_recon_state.recon_set.is_empty());
-        assert!(tx_recon_state.delayed_set.is_empty());
+        assert!(!tx_recon_state.recon_set);
+        assert!(!tx_recon_state.delayed_set);
 
-        // Add some txs to the recon set
-        for _ in 0..10 {
-            tx_recon_state.add_tx(get_random_txid());
-        }
+        // Add a transaction to the recon_set
+        tx_recon_state.add_tx();
 
-        // Check that all transactions are delayed
-        assert!(tx_recon_state.recon_set.is_empty());
-        assert!(!tx_recon_state.delayed_set.is_empty());
+        // Check that the transaction has been added to the delayed
+        // set, but the recon set remains empty
+        assert!(!tx_recon_state.recon_set);
+        assert!(tx_recon_state.delayed_set);
+        assert!(tx_recon_state.is_reconciling());
 
         // Move to available and check again
         tx_recon_state.make_delayed_available();
-        assert!(!tx_recon_state.recon_set.is_empty());
-        assert!(tx_recon_state.delayed_set.is_empty());
+        assert!(tx_recon_state.recon_set);
+        assert!(!tx_recon_state.delayed_set);
+        assert!(tx_recon_state.is_reconciling());
 
-        // Add some more transactions to delayed
-        for _ in 0..10 {
-            tx_recon_state.add_tx(get_random_txid());
-        }
-        assert!(!tx_recon_state.recon_set.is_empty());
-        assert!(!tx_recon_state.delayed_set.is_empty());
-
-        let txs = tx_recon_state.get_recon_set().clone();
-        let delayed_txs = tx_recon_state.delayed_set.clone();
-
-        // Clear and  check that the set returned is the one before clearing
-        // that the recon set is empty, but the delayed set is preserved
-        // and that the state is set as not reconciling anymore
-        assert_eq!(tx_recon_state.clear(), txs);
-        assert_eq!(tx_recon_state.delayed_set, delayed_txs);
-        assert!(tx_recon_state.recon_set.is_empty());
-        assert!(!tx_recon_state.delayed_set.is_empty());
+        // Clear, not including delayed (they are only included when cleaning after a simulation)
+        // and check that both sets are empty
+        tx_recon_state.clear(/*include_delayed=*/ false);
+        assert!(!tx_recon_state.recon_set);
+        assert!(!tx_recon_state.delayed_set);
         assert!(!tx_recon_state.is_reconciling());
+
+        // Add again, leave data in delayed and clear
+        tx_recon_state.add_tx();
+        tx_recon_state.clear(/*include_delayed=*/ true);
+        assert!(!tx_recon_state.recon_set);
+        assert!(!tx_recon_state.delayed_set);
+        assert!(!tx_recon_state.is_reconciling());
+
+        // If data is held in recon_set, delayed_set must be empty
+        // so not testing that case
     }
 
     #[test]
     fn test_sketch() {
+        // Start from an empty state
         let mut tx_recon_state = TxReconciliationState::new(true);
-        let mut our_txs = Vec::new();
-        let mut their_txs = Vec::new();
-        let mut unknown_by_us = Vec::new();
-        let mut unknown_by_them = Vec::new();
-        let d = 8;
 
-        // Add some txs to the recon set
-        for i in 0..10 {
-            let txid = get_random_txid();
-            tx_recon_state.recon_set.insert(txid);
-            our_txs.push(txid);
+        // Create their sketch without the transaction. Since none of us know the transaction, the diff size will be 0
+        let mut diff_size = 0;
+        let mut their_sketch = Sketch::new(false, diff_size);
+        assert!(their_sketch.get_size() == diff_size);
 
-            // Make half of the transactions also known by our peer (5)
-            if i % 2 == 0 {
-                their_txs.push(txid);
-            } else {
-                unknown_by_them.push(txid)
-            }
-        }
+        // Compute the diffs and check. None of us know the transaction so the diff should be false
+        let (our_diff, their_diff) = tx_recon_state.compute_sketch_diff(their_sketch);
+        assert!(!our_diff);
+        assert!(!their_diff);
 
-        // Create some transactions (3) unknown by us and add them to their set
-        for _ in 0..3 {
-            let txid = get_random_txid();
-            unknown_by_us.push(txid);
-            their_txs.push(txid);
-        }
+        // Add the tx to the recon set
+        tx_recon_state.add_tx();
+        tx_recon_state.make_delayed_available();
 
-        // Sort the vectors so they can be properly compared later
-        our_txs.sort();
-        their_txs.sort();
-        unknown_by_us.sort();
-        unknown_by_them.sort();
+        // Change their sketch, since now the difference will be 1
+        diff_size = 1;
+        their_sketch = Sketch::new(false, diff_size);
+        assert!(their_sketch.get_size() == diff_size);
 
-        let our_sketch = tx_recon_state.compute_sketch(their_txs.clone());
-        let mut sketch_txs = our_sketch.get_tx_set().clone();
-        sketch_txs.sort();
+        // Compute the diffs and check. We know the tx and they don't, so our diff should be false and theirs should be true
+        let (our_diff, their_diff) = tx_recon_state.compute_sketch_diff(their_sketch);
+        assert!(!our_diff);
+        assert!(their_diff);
 
-        // Check that our sketch contains the transactions we added to it
-        // and that it doesn't contain any of the transactions that only they had
-        assert_eq!(sketch_txs, our_txs);
-        for tx in their_txs.iter() {
-            if unknown_by_us.contains(tx) {
-                assert!(!our_sketch.get_tx_set().contains(tx))
-            }
-        }
-        // The set difference is 8: 5 (they're missing) + 3 (we're missing)
-        assert_eq!(our_sketch.get_size(), d);
+        // Update it so now we don't know but they do
+        tx_recon_state.clear(true);
+        their_sketch = Sketch::new(true, diff_size);
+        assert!(their_sketch.get_size() == diff_size);
 
-        // Compute their sketch and diff them
-        let their_sketch = Sketch {
-            tx_set: their_txs.clone(),
-            d,
-        };
-        let (mut our_diff, mut their_diff) = tx_recon_state.compute_sketch_diff(their_sketch);
-        our_diff.sort();
-        their_diff.sort();
-        assert_eq!(our_diff, unknown_by_us);
-        assert_eq!(their_diff, unknown_by_them);
+        // Compute the diffs and check. They know the transaction and we don't, so out diff should be true and theirs should be false
+        let (our_diff, their_diff) = tx_recon_state.compute_sketch_diff(their_sketch);
+        assert!(our_diff);
+        assert!(!their_diff);
+
+        // Update it so both of us know the transaction
+        diff_size = 0;
+        tx_recon_state.add_tx();
+        tx_recon_state.make_delayed_available();
+        their_sketch = Sketch::new(true, diff_size);
+
+        // Compute the diffs and check. We both know the transaction, so both diff should be false
+        let (our_diff, their_diff) = tx_recon_state.compute_sketch_diff(their_sketch);
+        assert!(!our_diff);
+        assert!(!their_diff);
     }
 }
